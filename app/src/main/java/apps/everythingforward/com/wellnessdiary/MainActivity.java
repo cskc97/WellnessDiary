91bd704375b9609c32c6af78aa4f59887248491d
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-03-13 00:03:37 +0800

    Improved UI

--- app/src/main/java/apps/everythingforward/com/wellnessdiary/MainActivity.java
+++ app/src/main/java/apps/everythingforward/com/wellnessdiary/MainActivity.java
@@ -1,14 +1,20 @@
 package apps.everythingforward.com.wellnessdiary;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+import com.victor.loading.book.BookLoading;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView mainBG;
+
 
     public static String bgURL = "https://source.unsplash.com/1600x900/?nature";
 
@@ -17,7 +23,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         mainBG = (ImageView)findViewById(R.id.mainBG);
+
+
+
+
 
 
 
@@ -27,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Picasso.with(getApplicationContext()).load(bgURL).into(mainBG);
+
+        Picasso.with(getApplicationContext()).load(bgURL).fit().into(mainBG);
+
+
+
     }
+
 }
