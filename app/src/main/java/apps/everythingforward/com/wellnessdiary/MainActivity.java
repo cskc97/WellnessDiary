a2a87a4f8918d598c3f35aaa4124ea9008b965d6
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-03-13 00:16:43 +0800

    Improved UI

--- app/src/main/java/apps/everythingforward/com/wellnessdiary/MainActivity.java
+++ app/src/main/java/apps/everythingforward/com/wellnessdiary/MainActivity.java
@@ -4,9 +4,12 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 import com.victor.loading.book.BookLoading;
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static String bgURL = "https://source.unsplash.com/1600x900/?nature";
 
+
+    ResideMenu resideMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +30,29 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
         mainBG = (ImageView)findViewById(R.id.mainBG);
 
+        resideMenu = new ResideMenu(this);
+        resideMenu.setBackground(R.drawable.bg);
+        resideMenu.attachToActivity(this);
+
+        String titles[] = {"Home" ,"My Diary","Mood Graph" };
+        int icon[] = {R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher};
+        for (int i = 0; i < titles.length; i++){
+            ResideMenuItem item = new ResideMenuItem(this, icon[i], titles[i]);
+            item.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            resideMenu.addMenuItem(item,  ResideMenu.DIRECTION_LEFT); // or  ResideMenu.DIRECTION_RIGHT
+        }
+
+
+        resideMenu.openMenu(ResideMenu.DIRECTION_LEFT);
 
 
 
@@ -42,8 +68,15 @@ public class MainActivity extends AppCompatActivity {
 
         Picasso.with(getApplicationContext()).load(bgURL).fit().into(mainBG);
 
+        // or ResideMenu.DIRECTION_RIGHT
 
 
+
+
+    }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return resideMenu.dispatchTouchEvent(ev);
     }
 
 }
