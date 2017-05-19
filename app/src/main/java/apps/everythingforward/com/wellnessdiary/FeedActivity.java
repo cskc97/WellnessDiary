f4e9b6ec67e887e910a0da1ea71f32c7e086e0d8
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-05-19 16:57:25 +0530

    Feed item

--- app/src/main/java/apps/everythingforward/com/wellnessdiary/FeedActivity.java
+++ app/src/main/java/apps/everythingforward/com/wellnessdiary/FeedActivity.java
@@ -3,11 +3,40 @@ package apps.everythingforward.com.wellnessdiary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
 public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+
+    public void getFeed()
+    {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("FeedItem");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }
